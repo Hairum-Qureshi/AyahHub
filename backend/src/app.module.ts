ba9a5005546d8659877b1cb6293baef529f3b594
Hairum Qureshi <hqureshi@udel.edu>
2025-12-11 18:10:01 -0500

    added Nest's ConfigModule to imports array so dotenv can run

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.MONGO_URI!)],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URI!),
+  ],
   controllers: [],
   providers: [],
 })
