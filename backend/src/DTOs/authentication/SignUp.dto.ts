82640ad60cfc4751f4c42c093e975f58da5df5bf
Hairum Qureshi <hqureshi@udel.edu>
2025-12-12 11:04:53 -0500

    added MinLength attribute to password fields in SignUp DTO

---
+++ backend/src/DTOs/authentication/SignUp.dto.ts
@@ -0,0 +1,25 @@
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+
+export class SignUpDto {
+  @IsNotEmpty()
+  @IsString()
+  firstName: string;
+
+  @IsNotEmpty()
+  @IsString()
+  lastName: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(6)
+  password: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(6)
+  confirmPassword: string;
+}
