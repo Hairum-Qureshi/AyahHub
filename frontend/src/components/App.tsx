c8ab0ee881329359bce154ca6d039c97636b3a63
Hairum Qureshi <hqureshi@udel.edu>
2025-12-14 00:48:25 -0500

    created routes

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,5 +1,15 @@
 import "../css/index.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Landing from "./pages/Landing";
+import NotFound from "./pages/NotFound";
 
 export default function App() {
-	return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Landing />} />
+				<Route path="*" element={<NotFound />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
