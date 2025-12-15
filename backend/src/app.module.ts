9c98b9b93bb224bda6e99dedbf80296596ac33e4
Hairum Qureshi <hqureshi@udel.edu>
2025-12-15 12:06:35 -0500

    removed user module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
-import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { FirebaseModule } from './firebase/firebase.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGO_URI!),
-    UserModule,
+    AuthModule,
+    FirebaseModule,
   ],
   controllers: [],
   providers: [],
