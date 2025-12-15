cca3418316d9b9cdb76910f10d7a9a59e61b12c5
Hairum Qureshi <hqureshi@udel.edu>
2025-12-15 18:13:24 -0500

    added CORS config

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,8 +4,13 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe()); // add this!
+  app.useGlobalPipes(new ValidationPipe());
   const PORT = process.env.PORT || 3000;
+
+  app.enableCors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  });
 
   await app.listen(PORT);
   console.log(`Application is running on: http://localhost:${PORT}`);
