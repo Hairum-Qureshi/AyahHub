2d60f44a30af9826d6e23087acd6ee3c44d13888
Hairum Qureshi <hqureshi@udel.edu>
2025-12-15 18:39:30 -0500

    created landing and not found endpoints for pages

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -2,10 +2,12 @@ import "../css/index.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Landing from "./pages/Landing";
 import NotFound from "./pages/NotFound";
+import Navbar from "../Navbar";
 
 export default function App() {
 	return (
 		<BrowserRouter>
+			<Navbar />
 			<Routes>
 				<Route path="/" element={<Landing />} />
 				<Route path="*" element={<NotFound />} />
