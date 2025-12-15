06b7255e6b3ef555bf4ca7e3b20e13c957564b26
Hairum Qureshi <hqureshi@udel.edu>
2025-12-15 17:36:57 -0500

    wrote method for getting all the Quranic chapters

--- backend/src/quran/quran.controller.ts
+++ backend/src/quran/quran.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { QuranService } from './quran.service';
+import chapters from '../datasets/chapters.json';
 
-@Controller('quran')
-export class QuranController {}
+@Controller('api/quran')
+export class QuranController {
+  constructor(private readonly quranService: QuranService) {}
+
+  @Get('all-chapters')
+  getChapters() {
+    return chapters;
+  }
+}
