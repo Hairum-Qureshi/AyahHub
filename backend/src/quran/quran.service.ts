0426e6b04acfd0f415b139d090465cfbcb84cb65
Hairum Qureshi <hqureshi@udel.edu>
2025-12-16 00:43:07 -0500

    created helper getJSONData method and moved logic from 'getChapter' so it's a reusable private method

--- backend/src/quran/quran.service.ts
+++ backend/src/quran/quran.service.ts
@@ -6,19 +6,27 @@ import path from 'path';
 export class QuranService {
   constructor() {}
 
-  async getChapter(chapterNumber: number, lang: 'en' | 'ur') {
-    const filePath = path.join(
+  private async getJSONData(
+    filePath: string,
+    chapterNumber: number,
+    lang?: 'en' | 'ur',
+  ) {
+    const joinedFilePath = path.join(
       process.cwd(),
-      'src',
-      'quran',
-      'datasets',
-      'chapters',
-      lang === 'en' ? 'en' : 'ur',
+      filePath,
+      lang ? lang : '',
       `${chapterNumber}.json`,
     );
+    return await readFile(joinedFilePath, 'utf-8');
+  }
 
+  async getChapter(chapterNumber: number, lang: 'en' | 'ur') {
     try {
-      const data = await readFile(filePath, 'utf-8');
+      const data = await this.getJSONData(
+        'src/quran/datasets/chapters',
+        chapterNumber,
+        lang,
+      );
       return data;
     } catch {
       throw new NotFoundException(`Surah ${chapterNumber} not found`);
