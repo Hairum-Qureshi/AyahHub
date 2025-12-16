3e62b80043db92f5f455dddc3d40ff34098b362a
Hairum Qureshi <hqureshi@udel.edu>
2025-12-15 23:34:35 -0500

    added useQuery hook call for getting chapter data by chapter number

--- frontend/src/hooks/useQuran.ts
+++ frontend/src/hooks/useQuran.ts
@@ -2,8 +2,8 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import type { UseQuranHookData } from "../interfaces";
 
-export default function useQuran(): UseQuranHookData {
-	const { data: chapterData } = useQuery({
+export default function useQuran(chapterNumber?: string): UseQuranHookData {
+	const { data: allQuranicChaptersData } = useQuery({
 		queryKey: ["chapters"],
 		queryFn: async () => {
 			try {
@@ -17,5 +17,21 @@ export default function useQuran(): UseQuranHookData {
 		}
 	});
 
-	return { chapterData };
+	const { data: chapterData } = useQuery({
+		queryKey: ["chapter", chapterNumber],
+		queryFn: async () => {
+			try {
+				const response = await axios.get(
+					`${
+						import.meta.env.VITE_BACKEND_URL
+					}/api/quran/chapter/${chapterNumber}`
+				);
+				return response.data;
+			} catch (error) {
+				console.error(error);
+			}
+		}
+	});
+
+	return { allQuranicChaptersData, chapterData };
 }
