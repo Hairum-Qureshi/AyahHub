fe67b7d04a217ed536fd2d1984a54cbd8c84c22a
Hairum Qureshi <hqureshi@udel.edu>
2025-12-15 23:36:29 -0500

    added 'getChapter' method that dynamically gets the appropriate JSON data source based on chapter number

--- backend/src/quran/quran.service.ts
+++ backend/src/quran/quran.service.ts
@@ -1,4 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { readFile } from 'fs/promises';
+import path from 'path';
 
 @Injectable()
-export class QuranService {}
+export class QuranService {
+  constructor() {}
+
+  async getChapter(chapterNumber: number, lang: 'en' | 'ur') {
+    const filePath = path.join(
+      process.cwd(),
+      'src',
+      'quran',
+      'datasets',
+      'chapters',
+      lang === 'en' ? 'en' : 'ur',
+      `${chapterNumber}.json`,
+    );
+
+    try {
+      const data = await readFile(filePath, 'utf-8');
+      return data;
+    } catch {
+      throw new NotFoundException(`Surah ${chapterNumber} not found`);
+    }
+  }
+}
