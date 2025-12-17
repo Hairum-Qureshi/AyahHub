22804c878b5459a52f96038056b32e5214ef317f
Hairum Qureshi <hqureshi@udel.edu>
2025-12-17 01:48:59 -0500

    created method for getting verse data and modified helper getJSONData method by incorporating type annotations

--- backend/src/quran/quran.service.ts
+++ backend/src/quran/quran.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { readFile } from 'fs/promises';
 import path from 'path';
+import { DetailedChapter, Verse } from 'src/interfaces';
 
 @Injectable()
 export class QuranService {
@@ -8,16 +9,18 @@ export class QuranService {
 
   private async getJSONData(
     filePath: string,
-    chapterNumber: number,
+    JSONFileName: number,
     lang?: 'en' | 'ur',
-  ) {
+  ): Promise<DetailedChapter> {
     const joinedFilePath = path.join(
       process.cwd(),
       filePath,
       lang ? lang : '',
-      `${chapterNumber}.json`,
+      `${JSONFileName}.json`,
     );
-    return await readFile(joinedFilePath, 'utf-8');
+
+    const fileContent: string = await readFile(joinedFilePath, 'utf-8');
+    return JSON.parse(fileContent) as DetailedChapter;
   }
 
   async getChapter(chapterNumber: number, lang: 'en' | 'ur') {
@@ -31,5 +34,35 @@ export class QuranService {
     } catch {
       throw new NotFoundException(`Surah ${chapterNumber} not found`);
     }
+  }
+
+  async getVerse(
+    chapterNumber: number,
+    verseNumber: number,
+    lang: 'en' | 'ur',
+  ): Promise<Verse> {
+    let data: DetailedChapter;
+
+    try {
+      data = await this.getJSONData(
+        'src/quran/datasets/chapters',
+        chapterNumber,
+        lang,
+      );
+    } catch {
+      throw new NotFoundException(`Surah ${chapterNumber} not found`);
+    }
+
+    const verse = data.verses.find((v) => {
+      return v.id === Number(verseNumber);
+    });
+
+    if (!verse) {
+      throw new NotFoundException(
+        `Verse ${verseNumber} of Surah ${chapterNumber} not found`,
+      );
+    }
+
+    return verse;
   }
 }
