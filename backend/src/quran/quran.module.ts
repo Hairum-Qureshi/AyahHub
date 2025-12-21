b995188e645e511ee34c49355cbacd2842fb8cf5
Hairum Qureshi <hqureshi@udel.edu>
2025-12-20 22:18:26 -0500

    added User and QuranNote schemas to imports

--- backend/src/quran/quran.module.ts
+++ backend/src/quran/quran.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { QuranService } from './quran.service';
 import { QuranController } from './quran.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { QuranNote, QuranNoteSchema } from 'src/schemas/QuranNote';
+import { User, UserSchema } from 'src/schemas/User';
 
 @Module({
   providers: [QuranService],
-  controllers: [QuranController]
+  controllers: [QuranController],
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: QuranNote.name,
+        schema: QuranNoteSchema,
+      },
+    ]),
+    MongooseModule.forFeature([
+      {
+        name: User.name,
+        schema: UserSchema,
+      },
+    ]),
+  ],
 })
 export class QuranModule {}
