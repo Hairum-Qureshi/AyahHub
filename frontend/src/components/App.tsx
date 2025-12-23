82e0ee44081791170497e34744fd7436c1cc44fd
Hairum Qureshi <hqureshi@udel.edu>
2025-12-23 00:04:19 -0500

    created NavWrapper that wraps components that will have a navbar so that pages that don't need a navbar won't have one

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -2,20 +2,25 @@ import "../css/index.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Landing from "./pages/Landing";
 import NotFound from "./pages/NotFound";
-import Navbar from "../Navbar";
 import QuranChapter from "./pages/QuranChapter";
+import NavWrapper from "./NavWrapper";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 
 export default function App() {
 	return (
 		<BrowserRouter>
-			<Navbar />
 			<Routes>
-				<Route path="/" element={<Landing />} />
-				<Route
-					path="/quran/chapter/:chapterNumber"
-					element={<QuranChapter />}
-				/>
-				<Route path="*" element={<NotFound />} />
+				<Route path="/sign-in" element={<SignIn />} />
+				<Route path="/sign-up" element={<SignUp />} />
+				<Route element={<NavWrapper />}>
+					<Route path="/" element={<Landing />} />
+					<Route
+						path="/quran/chapter/:chapterNumber"
+						element={<QuranChapter />}
+					/>
+					<Route path="*" element={<NotFound />} />
+				</Route>
 			</Routes>
 		</BrowserRouter>
 	);
