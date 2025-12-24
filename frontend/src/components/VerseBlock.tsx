626458da36fdbe5764061f7e58f783ea3cb50a76
Hairum Qureshi <hqureshi@udel.edu>
2025-12-24 01:56:44 -0500

    passed 'chpaterNumber' and 'verseNumber' as new additional props for NotesContainer component

--- frontend/src/components/VerseBlock.tsx
+++ frontend/src/components/VerseBlock.tsx
@@ -1,5 +1,7 @@
 import { useParams } from "react-router-dom";
 import type { VerseBlockProps } from "../interfaces";
+import NotesContainer from "./NotesContainer";
+import { useState } from "react";
 
 export default function VerseBlock({
 	verse,
@@ -7,36 +9,60 @@ export default function VerseBlock({
 	transliteration,
 	translation
 }: VerseBlockProps) {
-	function toArabicNumerals(num) {
+	function toArabicNumerals(num: number) {
 		const arabicNumbers = ["٠", "١", "٢", "٣", "٤", "٥", "٦", "٧", "٨", "٩"];
-		return num.toString().replace(/\d/g, d => arabicNumbers[d]);
+		return num
+			.toString()
+			.replace(/\d/g, d => arabicNumbers[d as unknown as number]);
 	}
 
 	const { chapterNumber } = useParams();
 
+	const [showNoteEditor, setShowNoteEditor] = useState(false);
+
+	const handleToggleNoteEditor = (show: boolean) => {
+		setShowNoteEditor(show);
+	};
+
+	// TODO - add styling to buttons in WYSIWYG editor to show which buttons are selected
+
 	return (
-		<div className="p-4 border border-yellow-500 rounded-md bg-[#041C2E]">
-			<div className="m-5">
-				<h3 className="text-right text-3xl font-secondary leading-15">
-					{verse}{" "}
-					<span className="text-yellow-500">
-						۝<span className="">{toArabicNumerals(verseNumber)}</span>
-					</span>
-				</h3>
-				<div className="mt-5">
-					<p className="text-left mt-2 text-lg text-yellow-500">
-						{transliteration}
-					</p>
-					<p className="text-left mt-2 text-lg">
-						{translation}.{" "}
+		<div className="border border-yellow-500 rounded-md bg-[#041C2E]">
+			<div className="p-4 flex flex-row">
+				<div className="flex-1 m-5">
+					<h3 className="text-right text-3xl font-secondary leading-15">
+						{verse}{" "}
 						<span className="text-yellow-500">
-							<span>
+							۝<span>{toArabicNumerals(verseNumber)}</span>
+						</span>
+					</h3>
+					<div className="mt-5">
+						<p className="text-left mt-2 text-lg text-yellow-500">
+							{transliteration}
+						</p>
+						<p className="text-left mt-2 text-lg">
+							{translation}.{" "}
+							<span className="text-yellow-500">
 								({chapterNumber}:{verseNumber})
 							</span>
-						</span>
-					</p>
+						</p>
+					</div>
+				</div>
+				<div className="border rounded-md w-10 bg-slate-800 border-sky-500 shadow-[0_0_8px_rgba(56,189,248,0.7)]">
+					<button
+						className="text-white"
+						onClick={() => handleToggleNoteEditor(!showNoteEditor)}
+					>
+						{showNoteEditor ? "Hide" : "Show"}
+					</button>
 				</div>
 			</div>
+			<NotesContainer
+				showNoteEditor={showNoteEditor}
+				handleToggleNoteEditor={handleToggleNoteEditor}
+				chapterNumber={chapterNumber as string}
+				verseNumber={verseNumber as unknown as string}
+			/>
 		</div>
 	);
 }
